d14a237908fe351fc6de7549d1796fc173fbe1b8
hninnayecho <hninnayecho@gmail.com>
2016-07-15 14:02:31 +0630

    initial

---
+++ opencv-starter/hninnayecho/hninnayecho.h
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+namespace hninnayecho{
+	void test();
+}
---
+++ opencv-starter/hninnayecho/test.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "hninnayecho/hninnayecho.h"
+#include <iostream>
+void hninnayecho::test(){
+	std::cout << "HninnAyeCho";
+
+}
