4f9cd06ebd706e329e88f7d91bd70d4fa07ad4e4
myatmoe <myat.moe.thwel@frobom.com>
2016-07-21 13:44:07 +0630

    add method comment

--- opencv-starter/MyatMoe/SmoothingProcessor.cpp
+++ opencv-starter/MyatMoe/SmoothingProcessor.cpp
@@ -20,6 +20,7 @@ smoothingProc::SmoothingProcessor::~SmoothingProcessor() {
 int::smoothingProc::SmoothingProcessor::HomogeneousSmoothing(string imagePath) {
 
 	prepareImgToSmooth(imagePath);
+	//void blur(InputArray src, OutputArray dst, Size ksize, Point anchor=Point(-1,-1), int borderType=BORDER_DEFAULT )
 	blur(src, dst, Size(5, 5));
 	displayImages("Homogenous Smoothing");
 	//waitKey(0);
@@ -28,6 +29,7 @@ int::smoothingProc::SmoothingProcessor::HomogeneousSmoothing(string imagePath) {
 
 int::smoothingProc::SmoothingProcessor::GaussianSmoothing(string imagePath) {
 	prepareImgToSmooth(imagePath);
+	//GaussianBlur(InputArray  src, OutputArray  dst, Size  ksize, double  sigmaX, double  sigmaY=0, int  borderType = BORDER_DEFAULT )
 	GaussianBlur(src, dst, Size(7, 7), 0, 0);
 	displayImages("Gaussian Smoothing");
 	//waitKey(0);
@@ -36,6 +38,7 @@ int::smoothingProc::SmoothingProcessor::GaussianSmoothing(string imagePath) {
 
 int::smoothingProc::SmoothingProcessor::MedianSmoothing(string imagePath) {
 	prepareImgToSmooth(imagePath);
+	//void medianBlur(InputArray src, OutputArray dst, int ksize)
 	medianBlur(src, dst, 5);
 	displayImages("Median Smoothing");
 	//waitKey(0);
@@ -44,6 +47,7 @@ int::smoothingProc::SmoothingProcessor::MedianSmoothing(string imagePath) {
 
 int::smoothingProc::SmoothingProcessor::BilateralSmoothing(string imagePath) {
 	prepareImgToSmooth(imagePath);
+	//void  bilateralFilter(InputArray src, OutputArray dst, int d, double sigmaColor, double sigmaSpace, intborderType=BORDER_DEFAULT)
 	bilateralFilter(src, dst, 11, 100, 100);
 	displayImages("Bilateral Smoothing");
 	//waitKey(0);
