ab0728b4367371b979293da77108a056fbe612de
su <su.myat.hlaing@frobom.com>
2016-07-25 17:14:03 +0630

    commit MouseEvent,Slider and TrackBar

--- opencv-starter/sumyat/SuMyat.h
+++ opencv-starter/sumyat/SuMyat.h
@@ -9,6 +9,10 @@ namespace sumyat{
 	static double thresh = 127.0;
 	static double maxVal = 255.0;
 	static int blockSize = 11;
+	const int alpha_slider_max = 100;
+	static int alpha_slider;
+	static double alpha;
+	static double beta;
 	static Mat img, src, dst1, dst2, dst3, dst4, dst5;
 	class ImageProcedure
 	{
@@ -16,7 +20,9 @@ namespace sumyat{
 		ImageProcedure();
 		~ImageProcedure();		
 		static void golbalThresholdTest(string imagePath);
-		static void adaptiveThresholdTest(string imagePath);		
+		static void adaptiveThresholdTest(string imagePath);	
+		static void mouseEventTest(string imagePath);
+		static void trackBarTest(string imagePath1,string imagePath2);
 
 	private:
 
