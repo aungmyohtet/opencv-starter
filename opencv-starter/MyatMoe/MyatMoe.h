fb2740dff528b3785cea3cff6ce75e718f13c289
myatmoe <myat.moe.thwel@frobom.com>
2016-07-28 13:58:20 +0630

    affine transformation

--- opencv-starter/MyatMoe/MyatMoe.h
+++ opencv-starter/MyatMoe/MyatMoe.h
@@ -1,6 +1,19 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
 namespace myatMoe {
 	void hi();
+	int testTutorial();
+
+	class AffineTranform
+	{
+	public:
+		AffineTranform();
+		~AffineTranform();
+		static void affineTransform(std::string imagePath);
+	};
 }
